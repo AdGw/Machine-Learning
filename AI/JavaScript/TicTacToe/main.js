01e5ee5d901c5af6d65912624b5b4dae3f925c78
Adgw <adrian.gwozdziej@gmail.com>
2020-01-02 10:30:06 +0100

    update tictactoe

--- AI/JavaScript/TicTacToe/main.js
+++ AI/JavaScript/TicTacToe/main.js
@@ -1,23 +1,64 @@
-function bestMove(){
-    let bestScore = -Infinity;
-    let bestMove;
-    for (let i = 0;i<3;i++){
-        for(let j = 0;j<3;j++){
-            if(board[i][j]=''){
-                board[i][j]=ai;
-                let score = minimax(board)
-                board[i][j] = '';
-                if(score > bestScore){
-                    bestScore = score;
-                    bestMove = {i,j};
-                }
-            }
+function bestMove() {
+  // AI to make its turn
+  let bestScore = -Infinity;
+  let move;
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      // Is the spot available?
+      if (board[i][j] == '') {
+        board[i][j] = ai;
+        let score = minimax(board, 0, false);
+        board[i][j] = '';
+        if (score > bestScore) {
+          bestScore = score;
+          move = { i, j };
         }
+      }
     }
-    board[bestMove.i][bestMove.j] = ai;
-    currentPlayer = human;
+  }
+  board[move.i][move.j] = ai;
+  currentPlayer = human;
 }
 
-function minimax(board){
-    return 1;
+let scores = {
+  X: 1,
+  O: -1,
+  tie: 0
+};
+
+function minimax(board, depth, isMaximizing) {
+  let result = checkWinner();
+  if (result !== null) {
+    return scores[result];
+  }
+
+  if (isMaximizing) {
+    let bestScore = -Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+        if (board[i][j] == '') {
+          board[i][j] = ai;
+          let score = minimax(board, depth + 1, false);
+          board[i][j] = '';
+          bestScore = max(score, bestScore);
+        }
+      }
+    }
+    return bestScore;
+  } else {
+    let bestScore = Infinity;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        // Is the spot available?
+        if (board[i][j] == '') {
+          board[i][j] = human;
+          let score = minimax(board, depth + 1, true);
+          board[i][j] = '';
+          bestScore = min(score, bestScore);
+        }
+      }
+    }
+    return bestScore;
+  }
 }
